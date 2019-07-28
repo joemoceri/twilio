9383bbefb1f52348670c55565959abe014d3e6fc
Joe Moceri <jmoceri34@gmail.com>
2019-07-28 00:28:28 -0400

    changes
    changes

---
+++ Twilio/Twilio/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Configuration;
+
+namespace TwilioExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var accountSid = ConfigurationManager.AppSettings["TwilioAccountSid"];
+            var authToken = ConfigurationManager.AppSettings["TwilioAuthToken"];
+
+            var twilioSample = new TwilioSample(accountSid, authToken);
+            var calls = twilioSample.GetCalls();
+
+
+        }
+    }
+}
---
+++ Twilio/Twilio/TwilioSample.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Twilio;
+
+namespace TwilioExample
+{
+    public interface ITwilioSample
+    {
+        IEnumerable<Call> GetCalls();
+    }
+
+    public class TwilioSample : ITwilioSample
+    {
+        private readonly string accountSid;
+        private readonly string authToken;
+
+        public TwilioSample(string accountSid, string authToken)
+        {
+            this.accountSid = accountSid;
+            this.authToken = authToken;
+        }
+
+        public IEnumerable<Call> GetCalls()
+        {
+            var client = new TwilioRestClient(accountSid, authToken);
+            var options = new CallListRequest();
+            options.StartTimeComparison = ComparisonType.GreaterThanOrEqualTo;
+            options.StartTime = DateTime.UtcNow.AddHours(-1);
+            var callResult = client.ListCalls(options);
+
+            if(callResult?.Calls == null || callResult.Calls.Count == 0)
+            {
+                return Enumerable.Empty<Call>();
+            }
+
+            return callResult.Calls;
+        }
+    }
+}
